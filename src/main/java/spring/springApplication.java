90eae0ac8c5c83409c3974b2b4b48b2dfec79760
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-23 22:19:14 +0900

    controller 통한 간단한 get

---
+++ src/main/java/spring/springApplication.java
@@ -0,0 +1,13 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(springApplication.class, args);
+    }
+
+}
